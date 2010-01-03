39b2d9e654d55da86bff13c809b837e07cb2c239
Vincent Isambart <vincent.isambart@gmail.com>
2010-01-03 10:31:06 +0900

    fixed a few tests

--- test.rb
+++ test.rb
@@ -82,9 +82,9 @@ UNICODE_ENCODINGS.each do |enc|
   case enc
   when :UTF_8
     assert_equal 27, data.bytesize
-  when :UTF_16LE, :UTF16_BE
+  when :UTF_16LE, :UTF_16BE
     assert_equal 18, data.bytesize
-  when :UTF_32LE, :UTF32_BE
+  when :UTF_32LE, :UTF_32BE
     assert_equal 36, data.bytesize
   end
 end
@@ -160,13 +160,20 @@ end
 UNICODE_ENCODINGS.each do |enc|
   data = read_data('cut', enc)
 
+  puts enc
   assert_equal false, data.valid_encoding?
+  if enc == :UTF_8
+    assert_equal 10, data.length
+  else
+    assert_equal 9, data.length
+  end
+
   case enc
-  when :UTF8
+  when :UTF_8
     assert_equal 26, data.bytesize
-  when :UTF16_BE, :UTF_16LE
+  when :UTF_16BE, :UTF_16LE
     assert_equal 17, data.bytesize
-  when :UTF32_BE, :UTF_32LE
+  when :UTF_32BE, :UTF_32LE
     assert_equal 35, data.bytesize
   end
 end
